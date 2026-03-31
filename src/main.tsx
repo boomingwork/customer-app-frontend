b43019e8a21159a039a1465f2b4a87bd2e0838f5
boomingwork <boomingwork@gmail.com>
2026-03-31 19:22:29 +0000

    fix backend api url

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 import { ApolloProvider } from "@apollo/client/react";
 
 const client = new ApolloClient({
-  link: new HttpLink({ uri: "creative-transformation-production-bc66.up.railway.app/graphql" }),
+  link: new HttpLink({ uri: "https://creative-transformation-production-bc66.up.railway.app/graphql" }),
   cache: new InMemoryCache(),
 });
 
